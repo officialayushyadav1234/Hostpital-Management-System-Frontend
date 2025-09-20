3672d45717629fa013d5edb325bebbf9a103bdb4
officialayushyadav1234 <officialayushyadav1234@gmail.com>
2025-09-20 22:35:33 +0530

    React Code

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+// src/App.jsx
+import React from "react";
+import { RouterProvider } from "react-router-dom";
+import { myRoutes } from "./ElRouter/Router";
+
+const App = () => {
+  return <RouterProvider router={myRoutes} />;
+};
+
+export default App;
